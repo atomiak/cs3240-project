0b9d329412207f7c5ea42af98636fa8539ae2023
Andrew <andrewtam0077@gmail.com>
2020-10-01 20:43:17 -0400

    added logout feature and user name on the nav bar

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
+
+
 from . import views
 
 app_name = 'home'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name = 'index')
+    path('', views.IndexView.as_view(), name = 'index'),
+    path('/accounts/logout', views.pagelogout, name = 'logout'),
 ]
