fccb162ee9017f0dcb010299dc592726b37b3362
Alex Tomiak <alex.m.tomiak@gmail.com>
2020-10-21 00:17:01 -0400

    tried to add fields to the Post model

--- events/models.py
+++ events/models.py
@@ -12,4 +12,6 @@ class Post(models.Model):
     post = models.CharField(max_length=500)
     user = models.ForeignKey(User, on_delete = models.DO_NOTHING)
     date = models.DateTimeField(auto_now=True)
+    date_of = models.DateTimeField()
+    event_description = models.CharField(max_length=500)
 
