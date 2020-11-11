1d773c6bd92e3e653761ac52958577513e569769
phools <rlw9ft@virginia.edu>
2020-11-11 16:30:47 -0500

    Adding the event date field

--- events/forms.py
+++ events/forms.py
@@ -5,5 +5,8 @@ class EventForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ('name', 'description', 'category', 'xcoordinate', 'ycoordinate',)
+        fields = ('name', 'description', 'category', 'xcoordinate', 'ycoordinate', 'event_date',)
+        widgets = {
+            'event_date': forms.DateInput(format=('%m/%d/%Y'), attrs={'class': 'form-control', 'placeholder': 'Select a date', 'type':'date'}),
+        }
 
