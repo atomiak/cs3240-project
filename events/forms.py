3bc75d3793f5ca670f2066a897fc9f7e2b264f51
phools <rlw9ft@virginia.edu>
2020-11-11 11:35:28 -0500

    Reverted before edits, Travis failing

--- events/forms.py
+++ events/forms.py
@@ -5,8 +5,5 @@ class EventForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ('name', 'description', 'category', 'xcoordinate', 'ycoordinate', 'event_date',)
-        widgets = {
-            'event_date': forms.DateInput(format=('%m/%d/%Y'), attrs={'class': 'form-control', 'placeholder': 'Select a date', 'type': 'date'}),
-        }
+        fields = ('name', 'description', 'category', 'xcoordinate', 'ycoordinate',)
 
