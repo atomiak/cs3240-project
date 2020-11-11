c00348b4fadd103183fc6cbf607ecbaf943e757c
Ben <bpl4vv@virginia.edu>
2020-11-11 12:50:16 -0500

    database change

--- meetup_finder/settings.py
+++ meetup_finder/settings.py
@@ -102,15 +102,21 @@ AUTHENTICATION_BACKENDS = [
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'postgres',
+#         'USER' : 'postgres',
+#         'PASSWORD' : '#2AD43240',
+#         'HOST' : '127.0.0.1',
+#         'PORT' : '5432',
+
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER' : 'postgres',
-        'PASSWORD' : '#2AD43240',
-        'HOST' : '127.0.0.1',
-        'PORT' : '5432',
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
