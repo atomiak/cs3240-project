498cba056c2694c9e97ddf98577d6e3629b6942c
Alex Tomiak <alex.m.tomiak@gmail.com>
2020-11-14 18:05:36 -0500

    tests (that pass locally) for EventForm, DetailView, and CreateView

--- events/tests.py
+++ events/tests.py
@@ -4,8 +4,10 @@ from django.test import TestCase
 from events.models import Post
 from django.contrib.auth.models import User
 from django.urls import reverse
+from events.forms import EventForm
+import datetime
 
-class PostFieldsTest(TestCase):
+class PostFieldsTest(TestCase):  
      def create_post(self, name="Test", description="A test event", category='Party', xcoordinate="1", ycoordinate="-1"):
          return Post.objects.create(name=name, description=description, user = User.objects.create(username='testusr'), category=category, xcoordinate=xcoordinate, ycoordinate=ycoordinate)
     
@@ -17,21 +19,9 @@ class PostFieldsTest(TestCase):
          self.assertEqual(p.category_to_text(), p.category)
          self.assertEqual(p.x_to_text(), p.xcoordinate)
          self.assertEqual(p.y_to_text(), p.ycoordinate)
-#    def test(self):
-#        self.assertFalse(1 == 2)
+
 
 class EventCreateView(TestCase):
-    # def setUpTestData():
-    #     number_of_posts = 5
-    #     for post_id in range(number_of_posts):
-    #         Post.objects.create(
-    #             name="Event "+str(post_id),
-    #             description = "test event " + str(post_id),
-    #             user = User.objects.create(username='testusr100'),
-    #             category = 'Party',
-    #             xcoordinate = "1",
-    #             ycoordinate = "-1"
-    #             )
     
     def test_view_url_exists_at_desired_location(self):
         response = self.client.get('/events/create/')
@@ -46,3 +36,46 @@ class EventCreateView(TestCase):
         response = self.client.get(reverse('events:create'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'events/create.html')
+
+class EventDetailView(TestCase):
+    def setUp(self):
+        user = User.objects.create(username='testusr01')
+
+        self.new_event = Post.objects.create(
+            name="Event 1",
+            description = "test event 1",
+            user = user,
+            category = 'Party',
+            xcoordinate = "1",
+            ycoordinate = "-1"
+            )
+    
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/events/', kwargs={'pk':1})
+        self.assertEqual(response.status_code, 200)
+    
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('events:detail', kwargs={'pk':self.new_event.pk}))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('events:detail', kwargs={'pk':self.new_event.pk}))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'events/detail.html')
+
+
+class EventFormTest(TestCase):
+    def test_event_form_date_today(self):
+        date = datetime.date.today()
+        form = EventForm(
+            data={
+                'name':'testname',
+                'description':'testdescription',
+                'category':'Party',
+                'xcoordinate':0,
+                'ycoordinate':0,
+                'event_date':date
+                }
+            )
+        self.assertTrue(form.is_valid())
+
